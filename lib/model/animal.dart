24647690836fd0fdf904348b7ea21ea3d554e218
VellxF <fidel.cavell@gmail.com>
2023-10-23 21:48:16 +0700

    Initial Commit

---
+++ lib/model/animal.dart
@@ -0,0 +1,8 @@
+class Animal {
+  List<String> photo;
+  String name;
+  String desc;
+  String tag;
+
+  Animal({required this.photo, required this.name, required this.desc, required this.tag});
+}
